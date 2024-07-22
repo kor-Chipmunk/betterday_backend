1739f6dcb10f57f630bdac553c5038173ece5df0
kor-Chipmunk <rhj4862@gmail.com>
2024-07-23 06:08:06 +0900

    feat: `ErrorCode` 추가

--- core/src/main/java/com/mashup/betterday/exception/ErrorCode.java
+++ core/src/main/java/com/mashup/betterday/exception/ErrorCode.java
@@ -8,11 +8,20 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
 
     UNKNOWN(500, "알 수 없는 오류입니다."),
+
+    USER_CREATE_FAILED(400, "유저 생성에 실패했습니다."),
     USER_NOT_FOUND(400, "유저를 조회할 수 없습니다."),
     LOGIN_FAILED(400, "로그인에 실패했습니다."),
 
-    DIARY_FAILED(400, "일기 작성에 실패했습니다."),
+    DIARY_CREATE_FAILED(400, "일기 작성에 실패했습니다."),
     DIARY_NOT_FOUND(400, "일기를 조회할 수 없습니다."),
+
+    ALARM_CREATE_FAILED(400, "알람 등록에 실패했습니다."),
+    ALARM_NOT_FOUND(400, "알람을 조회할 수 없습니다."),
+    ALARM_DUPLICATED(400, "알람이 이미 등록됐습니다."),
+
+    FCM_CREATE_FAILED(400, "FCM 등록에 실패했습니다."),
+    FCM_NOT_FOUND(400, "FCM을 조회할 수 없습니다."),
     ;
 
     private final int code;
