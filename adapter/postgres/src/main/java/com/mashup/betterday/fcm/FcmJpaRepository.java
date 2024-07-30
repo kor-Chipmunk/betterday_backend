ab8d9bac50adf0fcd64b426965e0f37129c38fbf
kor-Chipmunk <rhj4862@gmail.com>
2024-07-30 22:44:00 +0900

    fix: FCMEntity 삭제

--- adapter/postgres/src/main/java/com/mashup/betterday/fcm/FcmJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mashup.betterday.fcm;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FcmJpaRepository extends JpaRepository<FcmEntity, Long> {
-}
