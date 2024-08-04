f0fd2b83ceaca0a6952146fb08e5f1986f626d87
kor-Chipmunk <rhj4862@gmail.com>
2024-08-04 18:41:18 +0900

    fix: FCM 코드 제거

--- usecase/core/src/main/java/com/mashup/port/FcmPort.java
+++
@@ -1,9 +0,0 @@
-package com.mashup.port;
-
-import com.mashup.betterday.fcm.model.FCM;
-import com.mashup.betterday.fcm.model.FCMId;
-
-public interface FcmPort {
-    FCM save(FCM user);
-    FCM findById(FCMId id);
-}
