7f9981e26b603667a2d8239480b9812a972ec83d
kor-Chipmunk <rhj4862@gmail.com>
2024-08-13 02:15:16 +0900

    feat: setProperty 모듈 추가

--- api/external-api/src/main/java/com/mashup/betterday/ExternalApiApplication.java
+++ api/external-api/src/main/java/com/mashup/betterday/ExternalApiApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExternalApiApplication {
 
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "application, application-aes, application-fcm, application-jwt, application-oauth2, application-postgres, application-security");
+        System.setProperty("spring.config.name",
+                "application, application-aes, application-aws-sns, application-jasypt, application-fcm, application-jwt, application-oauth2, application-postgres, application-security, application-spring-event");
         SpringApplication.run(ExternalApiApplication.class, args);
     }
 
