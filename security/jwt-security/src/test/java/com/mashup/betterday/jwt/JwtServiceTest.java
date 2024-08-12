e50d8ae86c3a6e7d11201121030238170891338b
kor-Chipmunk <rhj4862@gmail.com>
2024-08-13 02:15:49 +0900

    test: 기본 테스트 코드 추가

---
+++ security/jwt-security/src/test/java/com/mashup/betterday/jwt/JwtServiceTest.java
@@ -0,0 +1,44 @@
+package com.mashup.betterday.jwt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JwtServiceTest {
+
+    @Test
+    void generateAccessToken() {
+    }
+
+    @Test
+    void generateRefreshToken() {
+    }
+
+    @Test
+    void getPayload() {
+    }
+
+    @Test
+    void getSubject() {
+    }
+
+    @Test
+    void testGenerateAccessToken() {
+    }
+
+    @Test
+    void testGenerateRefreshToken() {
+    }
+
+    @Test
+    void testGetPayload() {
+    }
+
+    @Test
+    void testGetSubject() {
+    }
+
+    @Test
+    void verifyRefreshToken() {
+    }
+}
