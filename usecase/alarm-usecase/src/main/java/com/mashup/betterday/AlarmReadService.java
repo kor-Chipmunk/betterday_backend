e10ff418209147561fdf54f838754a9011839ca8
kor-Chipmunk <rhj4862@gmail.com>
2024-09-07 01:57:09 +0900

    fix: Diary 단건 조회 시 예외 처리

--- usecase/alarm-usecase/src/main/java/com/mashup/betterday/AlarmReadService.java
+++ usecase/alarm-usecase/src/main/java/com/mashup/betterday/AlarmReadService.java
@@ -3,9 +3,7 @@ package com.mashup.betterday;
 import com.mashup.betterday.alarm.model.Alarm;
 import com.mashup.betterday.diary.model.Diary;
 import com.mashup.betterday.diary.model.DiaryId;
-import com.mashup.betterday.user.model.User;
 import com.mashup.port.AlarmPort;
-import com.mashup.port.DiaryPort;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,7 +16,11 @@ public class AlarmReadService implements AlarmReadUsecase {
 
     @Override
     public Alarm read(Diary diary) {
-        return alarmPort.findByDiary(diary.getId());
+        try {
+            return alarmPort.findByDiary(diary.getId());
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
